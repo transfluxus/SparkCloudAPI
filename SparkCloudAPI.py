26a88115d28fe12dddbf6151dae37945bf68924a
Ramin <ramin.soleymani@ixds.com>
2015-02-28 23:40:38 +0100

    changed access_token stuff added subscribe

--- SparkCloudAPI.py
+++ SparkCloudAPI.py
@@ -20,67 +20,74 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 '''
 
+import logging
+
 # pip install requests
 import requests
+from eventsource.client import EventSourceClient
+
+# set logger to DEBUG for detail returns
+logger = logging.getLogger("spark-logger")
+logger.setLevel(logging.INFO)
+logger.addHandler(logging.StreamHandler())
 
 class SparkCore(object):
-	# init
-	def __init__(self, user, password, deviceID):
-		self.user = user
-		self.password = password
-		self.deviceID = deviceID
+    # init
+    def __init__(self, user, password, deviceID, access_token=""):
+        self.user = user
+        self.password = password
+        self.deviceID = deviceID
+        self.access_token = access_token
+        if self.access_token == "":
+            self.access_token = self.getAccessToken()
+        self.deviceURL = 'https://api.spark.io/v1/devices/' + self.deviceID + '/'
 
-	# get an access token for your Spark Core
-	def getAccessToken(self):
-		# send post request and fetch output
-		postParameters = {'grant_type' : 'password', 'username' : self.user, 'password' : self.password}
-		r = requests.post('https://api.spark.io/oauth/token', auth = ('spark','spark'), data = postParameters)
+    # get an access token for your Spark Core
+    def getAccessToken(self):
+        # send post request and fetch output
+        postParameters = {'grant_type': 'password', 'username': self.user, 'password': self.password}
+        r = requests.post('https://api.spark.io/oauth/token', auth=('spark', 'spark'), data=postParameters)
+        json = r.json()
+        logger.debug("getAccessToken: "+str(json))
+        # returns access token
+        return json['access_token']
 
-		# returns access token
-		return r.json()['access_token']
+    # get list of all access tokens
+    def getAllTokens(self):
+        # send get request and return output
+        r = requests.get('https://api.spark.io/v1/access_tokens', auth=(self.user, self.password))
 
-	# get list of all access tokens
-	def getAllTokens(self):
-		# send get request and return output
-		r = requests.get ('https://api.spark.io/v1/access_tokens', auth = (self.user, self.password))
+        # returns a list of all your tokens
+        return r.text
 
-		# returns a list of all your tokens
-		return r.text
+    # delete an access token
+    def deleteToken(self, token):
+        # send delete request and return output
+        r = requests.delete('https://api.spark.io/v1/access_tokens/' + token, auth=(self.user, self.password))
 
-	# delete an access token
-	def deleteToken(self, token):
-		# send delete request and return output
-		r = requests.delete ('https://api.spark.io/v1/access_tokens/'+token, auth = (self.user, self.password))
+        # returns the response, should be 'true'
+        return r.text
 
-		# returns the response, should be 'true'
-		return r.text
+    # start a function, must be defined in your setup() function with Spark.function()
+    def sendFunctionRequest(self, functionName, functionParameter):
+        # define postParameters
+        postParameters = {'access_token': self.access_token, 'command': functionParameter}
+        # send post request and return output
+        r = requests.post(self.deviceURL + functionName, data=postParameters)
+        json = r.json()
+        logger.debug("sendFunctionRequest: "+str(json))
+        # returns an int
+        return json['return_value']
 
-	# start a function, must be defined in your setup() function with Spark.function()
-	def sendFunctionRequest(self, functionName, functionParameter):
-		# get a new temporary access token
-		tempToken = self.getAccessToken()
+    # read value of a variable, must be defined in your setup() function with Spark.variable()
+    def readVariable(self, variableName):
+        getParameters = {'access_token': self.access_token}
+        r = requests.get(self.deviceURL + variableName,params=getParameters)
+        json = r.json()
+        logger.debug("readVariable: "+str(json))
+        # returns the value of your variable
+        return json['result']
 
-		# define postParameters
-		postParameters = {'access_token' : tempToken, 'command' : functionParameter}
-
-		# send post request and return output
-		r = requests.post('https://api.spark.io/v1/devices/' + self.deviceID + '/' + functionName, data = postParameters)
-
-		# delet temporary access token
-		self.deleteToken(tempToken)
-
-		# returns an int
-		return r.text
-
-	# read value of a variable, must be defined in your setup() function with Spark.variable()
-	def readVariable(self, variableName):
-		# get a new temporary access token
-		tempToken = self.getAccessToken()
-
-		r = requests.get('https://api.spark.io/v1/devices/' + self.deviceID + '/' + variableName + '?' + 'access_token=' + tempToken)
-		
-		# delete temporary access token
-		self.deleteToken(tempToken)
-
-		# returns the value of your variable
-		return r.json()['result']
+    def subscribe(self,event_name,callback):
+        client = EventSourceClient("api.spark.io/v1/devices",self.deviceID+"/events",event_name+"?access_token="+self.access_token,callback,ssl=True,retry=8)
+        client.poll()
